3fcddc51a807a097ed03197c2457dcd46bef6c62
Aren Oganesyan <arem.h.y@tumo.org>
2024-06-24 20:50:52 +0400

    Added LinkedStack

---
+++ LinkedStack.java
@@ -0,0 +1,32 @@
+public class LinkedStack<T> implements Stack<T> {
+
+    private final LinkedList<T> list = new LinkedList<>();
+
+    public LinkedStack() {
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public void push(T item) {
+        list.addFirst(item);
+    }
+
+    @Override
+    public T top() {
+        return list.first();
+    }
+
+    @Override
+    public T pop() {
+        return list.removeFirst();
+    }
+}
